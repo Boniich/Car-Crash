1bd7371da0c733a368cc80203aca722a3368cf44
boniich <ezequieldbo22@hotmail.com>
2022-11-22 22:23:27 -0300

    feat: add restrictions to player movement

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,14 +12,23 @@ public class PlayerController : MonoBehaviour
     [Range(0, 50), SerializeField] private float turnSpeed = 0.0f;
 
     [SerializeField] private bool enablePhysicsEngine;
-    
+
+
+    [Space]
+
+    [SerializeField] private float rightXRange;
+    [SerializeField] private float leftXRange;
+    [SerializeField] private float forwardZRange;
+    [SerializeField] private float backZRange;
+
     private Rigidbody _rigidbody;
     private float horizontal, vertical;
     private Vector3 startPlayerPosition;
-    
+    private Vector3 recalculatePlayerPosition = new Vector3(0, 0, 0);
 
 
-     void Awake()
+
+    void Awake()
     {
         sharedInstance = this;
     }
@@ -40,6 +50,8 @@ public class PlayerController : MonoBehaviour
   
             vertical = Input.GetAxis("Vertical");
             horizontal = Input.GetAxis("Horizontal");
+
+            RestrictPlayerMovement();
 
             if (enablePhysicsEngine)
             {
@@ -70,6 +82,46 @@ public class PlayerController : MonoBehaviour
         }
 
         
+    }
+
+    /// <summary>
+    /// Restrict player movement to keep she in specific positions
+    /// </summary>
+
+
+    private void RestrictPlayerMovement()
+    {
+
+        if (transform.position.x < -leftXRange) {
+
+            recalculatePlayerPosition.Set(-leftXRange, transform.position.y, transform.position.z);
+
+            transform.position = recalculatePlayerPosition;
+        };
+
+        if (transform.position.x > rightXRange) {
+
+            recalculatePlayerPosition.Set(rightXRange, transform.position.y, transform.position.z);
+
+            transform.position = recalculatePlayerPosition;
+
+
+        };
+        if (transform.position.z < -backZRange) {
+
+            recalculatePlayerPosition.Set(transform.position.x, transform.position.y, -backZRange);
+
+            transform.position = recalculatePlayerPosition;
+
+        };
+
+        if (transform.position.z > forwardZRange) {
+
+            recalculatePlayerPosition.Set(transform.position.x, transform.position.y, forwardZRange);
+
+            transform.position = recalculatePlayerPosition;
+
+        } 
     }
 
     /// <summary>
